7fe9a18a7a5befc6f9035e51aa98ea4bc3ce496c
Lukas <lukasdaugela197@gmail.com>
2023-02-13 18:32:59 +0200

    Added font styles for button

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
 
-import { Search_icon } from '../public/assets/svg';
-import Button from './components/Button/Button';
-
 function App() {
   return (
     <>
       <div>App</div>
-      <Button type={'search'}>
-        <Search_icon />
-      </Button>
     </>
   );
 }
