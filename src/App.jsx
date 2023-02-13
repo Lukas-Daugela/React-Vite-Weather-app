62a6203721fba088ef3f41b02d401200b08dd69a
Lukas <lukasdaugela197@gmail.com>
2023-02-13 18:26:22 +0200

    Added "search" icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import { Search_icon } from '../public/assets/svg';
 import Button from './components/Button/Button';
 
 function App() {
   return (
     <>
       <div>App</div>
-      <Button type={'search'}>C</Button>
+      <Button type={'search'}>
+        <Search_icon />
+      </Button>
     </>
   );
 }
