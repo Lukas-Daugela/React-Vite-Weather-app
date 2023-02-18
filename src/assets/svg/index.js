21efd48cf944484157efeb94304c0994fc0818c1
Lukas <lukasdaugela197@gmail.com>
2023-02-18 09:07:14 +0200

    Changed title, deleted unnecessary lib

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -1,19 +1,19 @@
+import CloudyNight from './CloudyNight';
+import HumidityIcon from './HumidityIcon';
+import LocationIcon from './LocationIcon';
 import SearchIcon from './SearchIcon';
 import SunriseIcon from './SunriseIcon';
-import HumidityIcon from './HumidityIcon';
 import SunsetIcon from './SunsetIcon';
 import ThermometerIcon from './ThermometerIcon';
 import WindIcon from './WindIcon';
-import CloudyNight from './CloudyNight';
-import LocationIcon from './LocationIcon';
 
 export {
   CloudyNight,
-  SearchIcon,
-  WindIcon,
-  SunriseIcon,
-  SunsetIcon,
   HumidityIcon,
   LocationIcon,
+  SearchIcon,
+  SunriseIcon,
+  SunsetIcon,
   ThermometerIcon,
+  WindIcon,
 };
